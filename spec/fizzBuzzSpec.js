3efd1f25a8fa06598197e0ddb3314bd6eb6ce5e1
Christopher Ly <demedial@gmail.com>
2018-03-19 23:54:10 +0000

    Refactored src/fizzBuzz.js, all tests still green

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -25,12 +25,28 @@ describe('Fizzbuzz', function() {
     });
 
     it('is divisible by 3 and 5', function() {
-      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
     it('isn\'t divisible by 3 and 5', function() {
-      expect(fizzbuzz.isDivisibleByThreeAndFive(17)).toBe(false);
+      expect(fizzbuzz.isDivisibleByFifteen(17)).toBe(false);
     });
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Fizz" when a number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual("Fizz");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"Fizzbuzz" when a number is divisible by 15', function() {
+      expect(fizzbuzz.says(15)).toEqual("Fizzbuzz");
+    })
+
   });
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,15 +1,32 @@
 var Fizzbuzz = function() {
 
   Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy(number, 3);
   };
 
   Fizzbuzz.prototype.isDivisibleByFive = function(number) {
-    return (number % 5 === 0);
+    return this._isDivisibleBy(number, 5);
   };
 
-  Fizzbuzz.prototype.isDivisibleByThreeAndFive = function(number) {
-    return (number % 3 === 0 && number % 5 === 0);
+  Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+    return this._isDivisibleBy(number, 15);
+  };
+
+  Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+    return (number % divisor === 0);
+  };
+
+  Fizzbuzz.prototype.says = function(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "Fizzbuzz";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Fizz";
+    }
+    return number;
   };
 
 };
